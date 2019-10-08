b3f238bf09a8f18fe6bbdf628558a616dd983b26
Mohit Singh <45211271+devmohit-live@users.noreply.github.com>
2019-10-08 19:27:44 +0530

    Update pisano_period.py

--- pisano_period.py
+++ pisano_period.py
@@ -4,18 +4,18 @@ Pisano Period => length of the repeating sequence we got after getting modulo wi
 refrence : https://medium.com/competitive/huge-fibonacci-number-modulo-m-6b4926a5c836
 
 """
-pisano=[0,1]
+pisano=[]
 def pp(m):
 	a,b=0,1 # for getting fibonacci and this remains constant for every pisano period (0,1,.....)
 	 #=> it is found that max length can be atmost of m*m
-	for i in range(3,m*m):
+	for i in range(0,m*m):
 		c=(a+b)%m
 		a,b=b,(a+b)
 		pisano.append(c)
 		if a==0 and b==1:
 			break # ie sequence is repeatinf itself
 
-	print(pisano) # the actual sequence
+# 	print(pisano) # the actual sequence
 	return len(pisano) # or we can simply just have returned i+1 where we have written break
 
 
