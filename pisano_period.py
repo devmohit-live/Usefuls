295cca2b10601fa2a41d38d64f0f9cce91379954
Mohit Singh <45211271+devmohit-live@users.noreply.github.com>
2019-10-08 19:52:54 +0530

    Update pisano_period.py

--- pisano_period.py
+++ pisano_period.py
@@ -4,24 +4,40 @@ Pisano Period => length of the repeating sequence we got after getting modulo wi
 refrence : https://medium.com/competitive/huge-fibonacci-number-modulo-m-6b4926a5c836
 
 """
-pisano=[]
-def pp(m):
-	a,b=0,1 # for getting fibonacci and this remains constant for every pisano period (0,1,.....)
-	 #=> it is found that max length can be atmost of m*m
-	for i in range(0,m*m):
-		c=(a+b)%m
-		a,b=b,(a+b)
-		pisano.append(c)
-		if a==0 and b==1:
-			break # ie sequence is repeatinf itself
+# pisano=[]
+# def pp(m):
+# 	a,b=0,1 # for getting fibonacci and this remains constant for every pisano period (0,1,.....)
+# 	 #=> it is found that max length can be atmost of m*m
+# 	for i in range(0,m*m):
+# 		c=(a+b)%m
+# 		a,b=b,c
+# 		pisano.append(c)
+# 		if a==0 and b==1:
+# 			return i+1 # ie sequence is repeatinf itself
 
-# 	print(pisano) # the actual sequence
-	return len(pisano) # or we can simply just have returned i+1 where we have written break
+# 	 # the actual sequence
+# 	 # or we can simply just have returned i+1 where we have written break
+
+
+
+pisano=[0,1]
+def pisanoPeriod(m):
+    previous, current = 0, 1
+    for i in range(0, m * m):
+        res=(previous + current) % m
+        previous, current  = current, res
+        pisano.append(res)
+        if (previous == 0 and current == 1):
+            return i + 1
+
 
 
 if __name__=='__main__':
 	t=int(input('Enter no of cases : '))
 	for i in range(t):
 		m=int(input('Enter m : '))
-		print(pp(m))
+		res=pisanoPeriod(m)
+		print(res)
+		print(pisano)
+
 
